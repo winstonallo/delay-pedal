9a5b3404204a98e4d0bf85476648388fb472feb7
Eric Prototype <ericprottung@gmail.com>
2026-01-12 16:31:55 +0100

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Audio.h>
 
 AudioSynthWaveformSine sound;
+AudioSynthWaveformSine sound2;
 AudioControlSGTL5000 sgtl5000_1;
 AudioOutputI2S i2s1;
 AudioEffectEnvelope envelope;
 AudioConnection connect1(sound, 0, envelope, 0);
 AudioConnection connect2(sound, 0, envelope, 1);
+AudioConnection connect5(sound2, 0, envelope, 0);
+AudioConnection connect6(sound2, 0, envelope, 1);
 AudioConnection connect3(envelope, 0, i2s1, 0);
 AudioConnection connect4(envelope, 0, i2s1, 1);
 
@@ -13,9 +16,15 @@ void
 setup() {
     AudioMemory(8);
 
+
+
     sound.amplitude(0.5);
-    sound.frequency(440);
+    sound.frequency(110);
     sound.phase(180);
+    
+    sound2.amplitude(0.5);
+    sound2.frequency(220);
+    sound2.phase(180);
 
     envelope.attack(5);
     envelope.hold(50);
@@ -24,6 +33,8 @@ setup() {
 
     envelope.sustain(0);
     envelope.release(100);
+
+    
 
     sgtl5000_1.enable();
     sgtl5000_1.volume(0.7);
