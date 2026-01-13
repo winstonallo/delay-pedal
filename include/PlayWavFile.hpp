df555d2ce681b59807f4b4c11434840b6421cdfa
Arthur Bied-Charreton <arthur@biedcharreton.com>
2026-01-13 09:10:36 +0100

    Rename WavVolumeKnob to PlayWavFile and use VolumeKnob

---
+++ include/PlayWavFile.hpp
@@ -0,0 +1,32 @@
+#include "Config.hpp"
+
+#ifdef WVK
+
+#pragma once
+
+#include "VolumeKnob.hpp"
+#include <Audio.h>
+
+#define VOLUME_SAMPLES 5
+
+// 4.1 SD card
+#define SDCARD_CS_PIN BUILTIN_SDCARD
+#define SDCARD_MOSI_PIN 11 // not actually used
+#define SDCARD_SCK_PIN 13  // not actually used
+
+namespace pwf {
+
+extern AudioPlaySdWav playSdWav1;
+extern AudioOutputI2S i2s1;
+extern AudioConnection patchCord1;
+extern AudioConnection patchCord2;
+extern AudioControlSGTL5000 sgtl5000_1;
+
+extern VolumeKnob volume;
+
+void setup();
+void loop();
+
+} // namespace pwf
+
+#endif
---
+++ src/PlayWavFile.cpp
@@ -0,0 +1,63 @@
+#include "Config.hpp"
+
+#ifdef WVK
+
+#include "PlayWavFile.hpp"
+#include "VolumeKnob.hpp"
+#include <Audio.h>
+
+namespace pwf {
+
+AudioPlaySdWav playSdWav1;
+AudioOutputI2S i2s1;
+AudioConnection patchCord1(playSdWav1, 0, i2s1, 0);
+AudioConnection patchCord2(playSdWav1, 1, i2s1, 1);
+AudioControlSGTL5000 sgtl5000_1;
+
+VolumeKnob volume(A0, 100000);
+
+void
+setup() {
+    Serial.begin(9600);
+    AudioMemory(8);
+
+    pwf::sgtl5000_1.enable();
+    pwf::sgtl5000_1.volume(0.1);
+
+    SPI.setMOSI(SDCARD_MOSI_PIN);
+    SPI.setSCK(SDCARD_SCK_PIN);
+
+    while (!(SD.begin(SDCARD_CS_PIN))) {
+        Serial.println("Unable to access the SD card");
+        delay(500);
+    }
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    pwf::volume.begin([] {
+        float32_t vol = volume.update();
+        pwf::sgtl5000_1.volume(vol);
+    });
+
+    delay(1000);
+}
+
+void
+loop() {
+    if (pwf::playSdWav1.isPlaying() == false) {
+        Serial.println("Start playing");
+        pwf::playSdWav1.play("A.WAV");
+        delay(10);
+    }
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+
+    delay(100);
+}
+
+} // namespace pwf
+
+#endif
